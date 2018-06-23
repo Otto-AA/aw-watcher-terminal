e96cc16faed09e80b472ba7654807bfa49dd42ee
A_A <a-a@aa-Aspire-E5-771>
2018-06-23 22:27:17 +0200

    Delete background process after delay

--- aw-watcher-bash/aw-watcher-bash.sh
+++ aw-watcher-bash/aw-watcher-bash.sh
@@ -2,16 +2,18 @@
 
 
 # escape_quotes
-# Prepends a backslash to every quote and every backslash followed by a quote
+# Prepends a backslash to every quote (") and every backslash followed by a quote (\") or backslash (\\)
 #
 # From: echo "\"" > "output"
 # To:   echo \"\\\"\" > \"output\"
+# From: echo "\\" > "output"
+# To:   echo \"\\\\\" > \"output\"
 escape_quotes() {
     original="$1"
     length=${#original}
     result=""
 
-    # Iterate over chars and prepend a backslash to every quote and every backslash followed by a quote
+    # Iterate over chars and prepend a backslash to every quote and every backslash followed by a quote or a backslash
     for (( i=0; i<$length; i++ )); do
         char="${original:$i:1}"
 
@@ -21,7 +23,7 @@ escape_quotes() {
             next_char_index=$((i+1))
             if [[ $next_char_index -lt $length ]]; then
                 next_char="${original:$next_char_index:1}"
-                if [[ $next_char  = \" ]]; then
+                if [[ $next_char  = \" ]] || [[ $next_char = "\\" ]]; then
                     result+="\\"
                 fi
             fi
@@ -40,5 +42,12 @@ shell='bash'
 shell_version="$BASH_VERSION"
 pipe_path="/tmp/aw-watcher-terminal-pipe"
 
+message="--command \"$command\" --path \"$path\" --shell \"$shell\" --shell-version \"$shell_version\""
+
 # send message to pipe
-echo "--command \"$command\" --path \"$path\" --shell \"$shell\" --shell-version \"$shell_version\"" > "$pipe_path" &
+echo "$message" > "$pipe_path" &
+
+# The background process would keep open if the pipe was not opened for reading at the time of execution
+# Therefore delete background process after delay
+pipe_sender_pid="$!"
+sleep 5 && kill "$pipe_sender_pid" &> /dev/null &
