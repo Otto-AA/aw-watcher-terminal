1a3fe0316766c1a0c17998b70f8cf90a654933a7
A_A <a-a@aa-Aspire-E5-771>
2018-06-23 13:34:10 +0200

    Switch to logger and aw_core.log

--- aw-watcher-terminal.py
+++ aw-watcher-terminal.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-import os
 import argparse
+import logging
+import traceback
+import sys
+import os
 import shlex
 
 from time import sleep
 from datetime import datetime, timezone
 from aw_core.models import Event
+from aw_core.log import setup_logging
 from aw_client import ActivityWatchClient
 
 config = {
@@ -15,24 +19,33 @@ config = {
     'event_type': 'app.terminal.activity',
     'testing': True,
     'disabled': False,
-    'log_file': '/dev/null', # TODO: Change log file
+    'log_file': '/dev/null',  # TODO: Change log file
+    'verbose': True
 }
 client = None
 bucket_id = None
 parser = None
+logger = logging.getLogger(__name__)
 
 def main():
     load_config()
+
+    setup_logging(name="aw-watcher-terminal", testing=config['testing'],
+                verbose=config['verbose'], log_stderr=True, log_file=True)
+    
+    logger.info("Starting aw-watcher-terminal")
+
     init_client()
     init_message_parser()
     setup_named_pipe(config['pipe_path'])
     on_named_pipe_message(config['pipe_path'], handle_pipe_message)
 
+    logger.info("Listening for pipe messages...")
+
 
 def load_config():
     """[TODO] Load the configurations and parse the CLI arguments"""
-    print('Loaded configurations')
-    print(config)
+    pass
 
 
 def init_client():
@@ -42,10 +55,12 @@ def init_client():
 
     # Create client in testing mode
     client = ActivityWatchClient(config['client_id'], testing=config['testing'])
+    logger.info("Initialized AW Client")
 
     # Create Bucket if not already existing
     bucket_id = "{}_{}".format(config['bucket_name'], client.hostname)
     client.create_bucket(bucket_id, event_type=config['event_type'])
+    logger.info("Created bucket: {}".format(bucket_id))
 
 
 def init_message_parser():
@@ -57,54 +72,65 @@ def init_message_parser():
     parser.add_argument('--path', dest='path', help='the path of the shell')
     parser.add_argument('--shell', dest='shell', help='the name of the shell used')
     parser.add_argument('--shell-version', dest='shell_version', help='the version of the shell used')
-    print("Initialized argparser")
 
 
 def handle_pipe_message(message):
-    if config['disabled']:
-        return
-    
-    for line in message.split('\n'):
-        if not len(line):
-            continue
+    try:
+        if config['disabled']:
+            logger.debug("Skipping because watcher is disabled")
+            return
         
-        print('Received message: {}'.format(line))
-        try:
+        for line in message.split('\n'):
+            if not len(line):
+                continue
+            
+            logger.debug('Received message: {}'.format(line))
+
+            # Parse args
             args = parse_pipe_message(line)
-            argparse_dict = vars(args)
-        except:
-            return print("Error while parsing args")
-        
-        send_event(argparse_dict)
+            if not type(args) is argparse.Namespace:
+                logger.debug("Skipping because no arguments could be passed")
+                return
+            args_dict = vars(args)
+
+            send_event(args_dict)
+    except Exception as e:
+        logger.error("Unexpected Error: {}".format(e))
+        traceback.print_exc()
 
 
 def parse_pipe_message(message):
-    return parser.parse_args(shlex.split(message))
+    try:
+        return parser.parse_args(shlex.split(message))
+    except argparse.ArgumentError as e:
+        logger.error("Error while parsing args")
+        logger.error("Parse error: {}".format(e))
+        return None
 
 
 def send_event(event_data):
-    print("Sending event")
-    print(event_data)
+    logger.debug("Sending event")
+    logger.debug(event_data)
     now = datetime.now(timezone.utc)
     event = Event(timestamp=now, data=event_data)
     inserted_event = client.insert_event(bucket_id, event)
 
     # The event returned from insert_event has been assigned an id by aw-server
     assert inserted_event.id is not None
-    print("Successfully sent event\n")
+    logger.info("Successfully sent event")
 
 
 def setup_named_pipe(pipe_path):
     if os.path.exists(pipe_path):
         os.remove(pipe_path)
     if not os.path.exists(pipe_path):
-        print("Creating pipe {}".format(pipe_path))
+        logger.debug("Creating pipe {}".format(pipe_path))
         os.mkfifo(pipe_path)
 
 def on_named_pipe_message(pipe_path, callback):
     pipe_fd = os.open(pipe_path, os.O_RDONLY | os.O_NONBLOCK)
     with os.fdopen(pipe_fd) as pipe:
-        print("Listening to pipe: {}".format(pipe_path))
+        logger.info("Listening to pipe: {}".format(pipe_path))
         while True:
             message = pipe.read()
             if message:
